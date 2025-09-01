63359da7a59269d99e17db8e29fe9bfdd7f3fb67
ekaterina-abr <ekaterina.abr.99@mail.ru>
2025-09-01 23:48:36 +0300

    Add express app and destinations route for backend

---
+++ backend/src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { destinations } from "./config/destinations.js";
+
+const PORT = 3001;
+const app = express();
+
+app.get("/api/destinations", (request, response) => {
+  response.json(destinations);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ backend/src/types.ts
@@ -0,0 +1,14 @@
+export enum DestinationId {
+  Egypt = "Egypt",
+  Thailand = "Thailand",
+  Turkey = "Turkey",
+  UAE = "UAE",
+}
+
+export interface IDestination {
+  id: DestinationId;
+  title: string;
+  description?: string;
+  isSelected?: boolean;
+  isoCode?: string;
+}
