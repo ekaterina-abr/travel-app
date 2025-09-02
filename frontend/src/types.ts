25ca41027d5b7ead3ed05c74fbb2bae830eddb4a
ekaterina-abr <ekaterina.abr.99@mail.ru>
2025-09-02 22:33:57 +0300

    Remove unused interfaces

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,22 +1,7 @@
-export enum DestinationId {
-  Egypt = "Egypt",
-  Thailand = "Thailand",
-  Turkey = "Turkey",
-  UAE = "UAE",
-}
-
 export interface IDestinationResponse {
   id: string;
   title: string;
   description?: string;
   isSelected?: boolean;
   isoCode?: string;
-}
-
-export interface IDestination {
-  id: string;
-  title: string;
-  description?: string;
-  isSelected?: boolean;
-  flag?: React.ReactNode;
 }
